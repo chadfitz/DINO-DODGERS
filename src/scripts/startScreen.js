e287e84006d4697903e65c38430d29ca45cee8b6
chad fitzgerald <fitzchad@gmail.com>
2022-12-05 15:57:11 -0800

    Update startScreen background

--- src/scripts/startScreen.js
+++ src/scripts/startScreen.js
@@ -2,11 +2,15 @@ class StartScreen {
   constructor(canvas, ctx){
     this.canvas = canvas;
     this.ctx = ctx;
+    this.background = new Image();
+    this.background.src = "./src/backgrounds/start_screen_background.jpg"
   }
 
   renderStartScreen(){
-    background.onload = () => {
-      this.ctx.drawImage(background, 0, 0, this.canvas.width, this.canvas.height);
+    this.background.onload = () => {
+      // this.ctx.globalAlpha = 0.5;
+      this.ctx.drawImage(this.background, 0, 0, this.canvas.width, this.canvas.height);
+      // this.ctx.globalAlpha = 1;
     }    
   }
 }
